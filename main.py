93fec5ca737bf45236ce53f3a7d7b8197dd0fb9e
GreyShawn <greyshawn@pop-os.localdomain>
2021-05-15 20:28:13 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#invite people for the Kaggle party
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+
+df_train = pd.read_csv('train.csv')
+print(df_train.columns)
