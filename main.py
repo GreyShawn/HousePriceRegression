861bdbd2ff4b7a75f27f757993f1c00183e39550
GreyShawn <Simon.Takacs@hotmail.de>
2021-05-17 17:14:37 +0200

    added more Graphs

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ import warnings
 warnings.filterwarnings('ignore')
 
 df_train = pd.read_csv('train.csv')
+var = "GrLivArea"
+var2 = "TotalBsmtSF"
+dataLiv = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+dataBsm = pd.concat([df_train['SalePrice'], df_train[var2]], axis=1)
+
 print(df_train.columns)
 
-df_train['SalePrice'].describe()
+print(df_train['SalePrice'].describe())
 sns.displot(df_train['SalePrice'])
 print("Skewness: %f" % df_train['SalePrice'].skew()) 
 print("Kurtosis: %f" % df_train['SalePrice'].kurt())
-print("Test commit")
+dataLiv.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
+dataBsm.plot.scatter(x=var2, y='SalePrice', ylim=(0,800000))
 plt.show()
