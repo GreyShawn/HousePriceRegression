88eea352a3c6b2dc87896d711890f0ef05f68492
GreyShawn <Simon.Takacs@hotmail.de>
2021-05-17 17:34:23 +0200

    added Relationship with categorical variable OverallQual

--- main.py
+++ main.py
@@ -12,17 +12,30 @@ import warnings
 warnings.filterwarnings('ignore')
 
 df_train = pd.read_csv('train.csv')
-var = "GrLivArea"
-var2 = "TotalBsmtSF"
-dataLiv = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
-dataBsm = pd.concat([df_train['SalePrice'], df_train[var2]], axis=1)
-
 print(df_train.columns)
+
+#Facts about the most important variable
 
 print(df_train['SalePrice'].describe())
 sns.displot(df_train['SalePrice'])
 print("Skewness: %f" % df_train['SalePrice'].skew()) 
 print("Kurtosis: %f" % df_train['SalePrice'].kurt())
-dataLiv.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
-dataBsm.plot.scatter(x=var2, y='SalePrice', ylim=(0,800000))
+
+#Relationship with numerical variables
+
+var = "GrLivArea"
+var2 = "TotalBsmtSF"
+dataLiv = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
+dataBsm = pd.concat([df_train['SalePrice'], df_train[var2]], axis=1)
+#dataLiv.plot.scatter(x=var, y='SalePrice', ylim=(0,800000))
+#dataBsm.plot.scatter(x=var2, y='SalePrice', ylim=(0,800000))
+
+#Relationship withe categorical variables
+
+varOverAllQual = 'OverallQual'
+dataOverAllQual = pd.concat([df_train['SalePrice'], df_train[varOverAllQual]], axis=1)
+f,ax = plt.subplots(figsize=(8,6))
+fig = sns.boxplot(x=varOverAllQual, y="SalePrice", data = dataOverAllQual)
+fig.axis(ymin=0, ymax=800000)
+
 plt.show()
